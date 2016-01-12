e7ddc88dda2ba82e45b89a62fedee9fda77eadac
illus0r <zor667@gmail.com>
2016-01-12 19:13:05 +0100

    Squashed commit of the following:
    commit 80ea441a3a495b337e74f5839bf660f7ba486091
    Merge: b4bb05d 24db56b
    Author: illus0r <zor667@gmail.com>
    Date:   Tue Jan 12 21:11:52 2016 +0300
    
        #97 improving js navigation by hash tags - 2
    
    commit 24db56bc5d67644fd1b708c1dcedb1c7331aeac8
    Author: illus0r <zor667@gmail.com>
    Date:   Tue Jan 12 20:54:10 2016 +0300
    
        #97 improving js navigation by hash tags
    
    commit bd3d9d76db975f8d1a9c4e1fb796c08ee66f3695
    Author: illus0r <zor667@gmail.com>
    Date:   Sun Jan 10 19:44:57 2016 +0300
    
        #97 switched to js filtering method. in progress
    
    commit a691aebf2283529080063399a8cdead51772b45a
    Author: illus0r <zor667@gmail.com>
    Date:   Sun Jan 10 17:06:16 2016 +0300
    
        index on 97-js-fitered-blog: a531953 #59 forgot about margin-bottom for mobile

--- javascripts/blog-filtering.js
+++ javascripts/blog-filtering.js
@@ -1,4 +1,16 @@
 function filter(currentTag){
+  var body = $("html, body");
+  body.stop().animate({scrollTop:0}, '200', 'swing');
+
+  // if hash is empty
+  //
+  if (currentTag == "" && window.location.pathname == "/blog/") {
+    currentTag = "Featured";
+    console.log("currentTag = " + currentTag);
+  }
+
+  // Setting visibility
+  //
   $(".layout-tag-page h1").text(currentTag);
   $(".layout-tag-page article").filter(function( index ) {
     return !$( this ).data("tags").includes(currentTag);
@@ -7,25 +19,43 @@ function filter(currentTag){
   var articlesToShow = $(".layout-tag-page article").filter(function( index ) {
     return $( this ).data("tags").includes(currentTag);
   });
-  
   articlesToShow.show("fast");
-  console.log(articlesToShow.length);
-  //if ( (".layout-tag-page article:visible").length == 0 ){
-    //console.log("nothing found");
-  //}
+
+  // Highlighting links
+  //
+  $( "#search a" ).filter(function( index ) {
+    return $( this ).text() != currentTag;
+  })
+  .removeClass("active");
+  $( "#search a" ).filter(function( index ) {
+    return $( this ).text() == currentTag;
+  })
+  .addClass("active");
+  
+  // Wrong hash
+  //
+  if(articlesToShow.length == 0 && window.location.pathname == "/blog/"){
+    window.location.replace("/404.html");
+  }
 }
 
-$( "#search a" ).click(function() {
-  var currentTag = $(this).text();
+//$( "#search a" ).click(function() {
+  //var currentTag = $(this).text();
+  //filter(currentTag);
+//});
+
+
+$(window).on('hashchange', function() {
+  currentTag = window.location.hash.slice(1);
   filter(currentTag);
 });
 
 $(function() {
+  // Put *Featured* right after *Newest*
+  $( "#search a" ).filter(function( index ) {
+    return $( this ).text() == "Featured";
+  }).insertAfter("#search a:first");
+
   currentTag = window.location.hash.slice(1);
-  console.log("currentTag = " + currentTag);
-  if (currentTag == "") {
-    currentTag = "Featured";
-    console.log("currentTag = " + currentTag);
-  }
   filter(currentTag);
 });
