8c2ecd0fce92dc9ba5e0bc628fb5e95f6c8b99ff
Lars Kruse <lak@praqma.net>
2016-01-20 16:17:21 +0100

    Closed #98 google analytics

--- javascripts/googleanalytics.js
+++ javascripts/googleanalytics.js
@@ -1,8 +1,7 @@
-  //(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  //(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  //m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  //})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  //ga('create', 'UA-33979353-1', 'praqma.com');
-  //ga('send', 'pageview');
-  // TODO: Make it online when we go live!
+  ga('create', 'UA-33979353-1', 'www.praqma.com');
+  ga('send', 'pageview');
